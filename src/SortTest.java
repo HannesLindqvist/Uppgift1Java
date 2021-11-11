690f23f2bf88275c618c1bd0d9c25f2229678e07
HannesLindqvist <Hannes.Lindqvist@outlook.com>
2021-11-11 16:21:09 +0100

    Skalet för uppgiften

---
+++ src/SortTest.java
@@ -0,0 +1,5 @@
+public class SortTest {
+    public static void main(String[] args){
+        //En testfil för att testa koden senare
+    }
+}
---
+++ src/Sorter.java
@@ -0,0 +1,6 @@
+public class Sorter {
+    public static int [] sort(int [] unsorted){
+        return unsorted;
+        //Skriv en algoritm som sorterar ints från lägsta till högsta talet.
+    }
+}

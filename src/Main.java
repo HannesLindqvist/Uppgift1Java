9e467bb93eb2a34c6be19fb96e544c82832f7434
HannesLindqvist <Hannes.Lindqvist@outlook.com>
2021-11-16 14:59:49 +0100

    Började om. Gjorde en arraylista och en forloop för att loopa args

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hej");
+        int[] num = new int[args.length];
+
+
+        for(int i = 0; i<args.length; i++) {
+            System.out.println("args[" + i + "]: " + args[i]);
+
+        }
     }
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,22 +1,6 @@
-import java.util.Arrays;
-import java.util.Random;
-
-public class Sorter {
-    public static int[] sor(){
 
 
-        int [] array = new int [10];
-        Random rand = new Random();
-        for (int i = 0; i < array.length; i++)
-            array[i] = rand.nextInt(-100, 100) +1;
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
 
-
-        return array;
-
-    }
-}
 
 
 

37814b4666771e5729e269322bdbf4705a87fda1
HannesLindqvist <Hannes.Lindqvist@outlook.com>
2021-11-16 13:48:50 +0100

    Addat sorteringsalgoritmen

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Sorter {
-    public static int [] sort(int [] unsorted){
-        return unsorted;
-        //Skriv en algoritm som sorterar ints från lägsta till högsta talet.
+    public static int[] sor(){
+
+
+        int [] array = new int [10];
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++)
+            array[i] = rand.nextInt(-100, 100) +1;
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+
+        return array;
+
     }
 }
+
+
+
